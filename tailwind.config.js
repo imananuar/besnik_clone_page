3c9a93e0394f75fceab2bb499e38c7d3d70bd3a0
mianuar <53685685+imananuar@users.noreply.github.com>
2022-02-15 15:53:22 +0800

    Deal & Features, Philosophy Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js", "./public/index.html"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'card': "url('assets/card.png')",
-      }
-    },
+    fontFamily: {
+      'bai-jamjuree': ['Bai Jamjuree', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+      'inter': ['Inter', 'sans-serif'],
+    }
   },
   plugins: [],
 }
