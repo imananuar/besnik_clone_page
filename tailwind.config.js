3328c2d77558516f954328a2247b509724882421
mianuar <53685685+imananuar@users.noreply.github.com>
2022-02-16 09:12:47 +0800

    Feature Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
       'bai-jamjuree': ['Bai Jamjuree', 'sans-serif'],
       'roboto': ['Roboto', 'sans-serif'],
       'inter': ['Inter', 'sans-serif'],
+    },
+
+    letterSpacing: {
+      'little-wide': '0.04',
+      'wider': '.05em',
+      'even-wider': '.28em',
+    },
+
+    extend: {
+      dropShadow: {
+        '4xl': '0 35px 35px rgba(207,237,255,1)',
+      }
     }
   },
   plugins: [],
